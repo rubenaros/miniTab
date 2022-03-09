9c144844e1d1e645ae48162e8830238ec3004c07
rubenaros <rubenaros@gmail.com>
2022-03-08 23:01:59 -0300

    Initialized the repo with cookiecutter data science structure

---
+++ miniTab/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Fork de GFTE',
+    author='Rubén Aros',
+    license='',
+)
